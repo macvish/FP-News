006e4673631d9b095d18aa0554a5d42cb133979c
Macanthony <macintony1@gmail.com>
2022-07-09 14:41:18 +0100

    Finalize navigation setup

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import codePush from 'react-native-code-push'
 
 import AppNavigation from './navigators/app'
 
@@ -14,4 +15,4 @@ const App: React.FC = () => {
   )
 }
 
-export default App
+export default codePush(App)
