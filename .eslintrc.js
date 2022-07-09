591eaf1f6973eec748b69ed4bd259bdf98f1e3fe
Macanthony <macintony1@gmail.com>
2022-07-09 07:56:50 +0100

    add needed packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,23 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        strict: 0,
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'no-whitespace-before-property': 2,
+        'no-unused-vars': 1,
+        'react/prop-types': 0,
+        'react/jsx-key': 1,
+        'react/react-in-jsx-scope': 0,
+        'react/display-name': 0,
+        'no-unreachable': 1,
+        'use-isnan': 1,
+        'dot-notation': 1,
+        eqeqeq: 2,
+        'no-extra-bind': 2,
+        'no-empty-function': 1,
+        'no-restricted-imports': 0
       },
     },
   ],
